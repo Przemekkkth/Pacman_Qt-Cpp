cd847bbf6b315c2be5371e93aa90c612a94da96c
Przemek Badaruk <przemekkth@outlook.com>
2022-08-09 08:10:02 +0200

    Restructure res files.

--- src/resources.cpp
+++ src/resources.cpp
@@ -2,8 +2,8 @@
 #include <QDebug>
 
 
-const QString Resources::PATH_TO_LABYRINTH_PIXMAP = ":/res/labyrinth.png";
-const QString Resources::PATH_TO_THINGS_PIXMAP    = ":/res/things.png";
+const QString Resources::PATH_TO_LABYRINTH_PIXMAP = ":/res/sprite/labyrinth.png";
+const QString Resources::PATH_TO_THINGS_PIXMAP    = ":/res/sprite/things.png";
 const QString Resources::PATH_TO_FONT             = ":/res/font/arcadepi.ttf";
 
 Resources::Resources()
